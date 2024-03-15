097c26c3cfdb87d4bf1d786a6f05c57e6a5e771f
Nguyen <NguyenCNTT08@gmail.com>
2024-03-15 15:12:30 +0700

    fix-header

--- inc/header.php
+++ inc/header.php
@@ -47,11 +47,41 @@
           <div
             class="header-top d-flex justify-content-between align-items-lg-center navbar-expand-lg"
           >
-            <div class="col menu-left">
+            <!-- <div class="col menu-left">
               <a class="active" href="index.php">Home</a>
               <a href="category.php">Category</a>
               <a href="archive.php">Archive</a>
-            </div>
+            </div> -->
+            <nav class="col navbar navbar-expand-lg justify-content-end">
+              <!-- Navbar links -->
+              <div
+                class="collapse navbar-collapse menu-right"
+                id="collapsibleNavbar"
+              >
+                <ul class="navbar-nav justify-content-center w-100">
+                  
+                  
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                  </li> 
+                  <li class="nav-item">
+                    <a class="nav-link" href="category.php">Category</a>
+                  </li>
+                  <?php
+                  if(Auth::isLoggedIn()){
+                    $user = $_SESSION['user'];
+                    $roles = $user->role;
+                    if(in_array('USER', $roles)){
+                      echo '<li class="nav-item">' 
+                      .'<a class="nav-link" href="./textEditor/index.php">Viết bài</a>'.
+                      '</li>';
+                    }
+                  }
+                  ?>
+                  
+                </ul>
+              </div>
+            </nav>
             <div class="col-5 text-lg-center mt-2 mt-lg-0">
               <span class="logo-outer">
                 <span class="logo-inner">
@@ -62,54 +92,15 @@
               </span>
             </div>
             <nav class="col navbar navbar-expand-lg justify-content-end">
-              <!-- Toggler/collapsibe Button -->
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#collapsibleNavbar"
-              >
-                <span class="lnr lnr-menu"></span>
-              </button>
-
               <!-- Navbar links -->
               <div
                 class="collapse navbar-collapse menu-right"
                 id="collapsibleNavbar"
               >
                 <ul class="navbar-nav justify-content-center w-100">
-                  
-                  <li class="nav-item hide-lg">
-                    <a class="nav-link" href="index.php">Home</a>
-                  </li>
-                  <li class="nav-item hide-lg">
-                    <a class="nav-link" href="category.php">Category</a>
-                  </li>
-                  <!-- Dropdown -->
-                  <!-- <li class="nav-item dropdown">
-                    <a
-                      class="nav-link dropdown-toggle"
-                      href="#"
-                      id="navbardrop"
-                      data-toggle="dropdown"
-                    >
-                      Pages
-                    </a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="elements.php">Elements</a>
-                    </div>
-                  </li> -->
-                  <!-- <li class="nav-item">
-                    <a class="nav-link" href="elements.php">Elements</a>
-                  </li> -->
                   <?php
                   if(Auth::isLoggedIn()){
-                    $user = $_SESSION['user'];
-                    $roles = $user->role;
                     if(in_array('USER', $roles)){
-                      echo '<li class="nav-item">' 
-                      .'<a class="nav-link" href="./textEditor/index.php">Viết bài</a>'.
-                      '</li>';
                       echo '<li class="nav-item">' 
                       .'<a class="nav-link" href="./user/index.php">Profile</a>'.
                       '</li>';
@@ -126,9 +117,9 @@
                   <!-- <li class="nav-item">
                     <a class="nav-link" href="blog-single.php">Blog Detail</a>
                   </li> -->
-                  <!-- <li class="nav-item">
+                  <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact</a>
-                  </li> -->
+                  </li>
                   <?php
                   if(Auth::isLoggedIn()){
                     echo '<li class="nav-item">'

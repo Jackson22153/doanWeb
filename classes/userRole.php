5579343d602d3d7ecb29682058b229c580f5cd16
Nguyen <NguyenCNTT08@gmail.com>
2024-03-15 10:15:03 +0700

    adduserrole

--- classes/userRole.php
+++ classes/userRole.php
@@ -7,7 +7,13 @@
             $this->userID = $userID;
             $this->roleID = $roleID;
         }
-
+        public function addUserRole($conn){
+            $sql = "INSERT INTO usersroles (userID, roleID) VALUES (:userID, :roleID)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':userID', $this->userID, PDO::PARAM_INT);
+            $stmt->bindValue(':roleID', $this->roleID, PDO::PARAM_INT);
+            return $stmt->execute();
+        }
         
     }
 

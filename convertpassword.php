2f89d4e8a20224775d6dee4612dbf554e4156294
Jackson22153 <phucxauxi929@gmail.com>
2024-03-15 21:22:07 +0700

    update

--- convertpassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require './inc/init.php';
-    $user = new User("admin123", 'admin123');
-    $conn = require './inc/db.php';
-    if($user->addUser($conn)){
-        echo 'add user successfully';
-    }else echo 'add user failed';
-
-?>

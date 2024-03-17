ff949bbc8b65725af8474551c259218ef68f83cd
Jackson22153 <phucxauxi929@gmail.com>
2024-03-17 17:10:50 +0700

    github login style button

--- login.php
+++ login.php
@@ -39,6 +39,8 @@
         <title>Login</title>
         <!-- Reset CSS -->
         <link rel="stylesheet" href="./css/reset.css" />
+        <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+
 
 
         <!-- Styles -->
@@ -81,15 +83,14 @@
                     </a>
                 </div>
                 <div class="media-options">
-                    <a href="github-oauth.php" class="facebook-login-btn">
+                    <a href="github-oauth.php" class="github-login-btn">
                         <span class="icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"/></svg>
+                            <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub Logo">
                         </span>
-                        <div class="desc"><p>Login with Facebook</p></div>
+                        <div class="desc"><p>Login with Github</p></div>
                     </a>
                 </div>
-            </div>
-            
+            </div>            
         </section>
     </body>
 </html>
